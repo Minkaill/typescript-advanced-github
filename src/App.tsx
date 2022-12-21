fd1ed06f97adda78ba0d3ff3acb044787ebff367
Wiynxr <mkx.dake@gmail.com>
2022-12-21 18:29:28 +0300

    Подключил tailwind, настроил проект

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Routes, Route} from "react-router-dom"
+import FavoritesPage from './pages/FavoritesPage';
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='/favorites' element={<FavoritesPage />} />
+    </Routes>
+  );
+}
+
+export default App;
