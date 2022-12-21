54a5ff2967da90f93cee6450ba4b3f37386fa44f
Wiynxr <mkx.dake@gmail.com>
2022-12-21 19:01:24 +0300

    Подключил RTKQuery по запросу

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -4,10 +4,10 @@ import {Link} from 'react-router-dom'
 const Navigation = () => {
   return (
       <nav className='flex justify-between items-center h-[50px] px-5 shadow-md bg-gray-500 text-white' >
-          <h3>Github Search</h3>
+          <h3 className='font-bold' >Github Search</h3>
 
           <span>
-              <Link to="/">Home</Link>
+              <Link to="/" className='mr-2'>Home</Link>
               <Link to="/favorites">Favorites</Link>
           </span>
       </nav>
