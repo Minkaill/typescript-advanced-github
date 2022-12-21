0d0b2697700142ad9149bb4f74b482f9bf9c5acc
Wiynxr <mkx.dake@gmail.com>
2022-12-21 21:36:07 +0300

    Добавил Debounced, кастомный хук

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import { useSearchUsersQuery } from '../store/github/github.api'
+import React from "react";
+import { useSearchUsersQuery } from "../store/github/github.api";
 
 const HomePage = () => {
-  const {isLoading, isError, data} = useSearchUsersQuery("Minkail")
+  const { isError, isLoading, data } = useSearchUsersQuery("Minkail");
+  const [search, setSearch] = React.useState("")
 
-  console.log(data)
+
+  React.useEffect(() => {
+    
+  }, [search])
+
+
   return (
-    <div>
-      HomePage
-    </div>
-  )
-}
+    <div className="flex justify-center pt-10 mx-auto h-screen w-screen">
+      {isError && <p className="text-center text-red-600"> Ошибка</p>}
 
-export default HomePage
+      <div className="relative w-[560px]">
+        <input
+          value={search}
+          type="text"
+          className="border py-2 px-4 w-full h-[42px] mb-2"
+          placeholder="Search for github Username..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <div className="absolute top-[42px] left-0 right-0 max-h-[200px] shadow-md bg-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          totam aliquam provident natus deleniti maiores vel ipsam earum
+          cupiditate nostrum consequuntur ad recusandae laboriosam excepturi
+          officiis, quia expedita quod eum?
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,10 @@
-import {configureStore} from "@reduxjs/toolkit"
-import { githubApi } from './github/github.api';
-
+import { configureStore } from "@reduxjs/toolkit";
+import { githubApi } from "./github/github.api";
 
 export const store = configureStore({
-    reducer: {
-        [githubApi.reducerPath]: githubApi.reducer
-    }
-})
+  reducer: {
+    [githubApi.reducerPath]: githubApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(githubApi.middleware),
+});
